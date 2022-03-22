47a7e0c707a7bc4eadce999f56334106688ac9b9
Niall Cooling <niall.cooling@gmail.com>
2022-03-22 21:19:09 +0000

    signed and unsigned working

--- src/efmt.hpp
+++ src/efmt.hpp
@@ -3,34 +3,38 @@
 #include <cstdio>
 #include <string_view>
 
+// macro to allow redirection of output
 #ifndef Putchar
 #define Putchar(x) putchar(x)
 #endif
 
 namespace embedded::fmt {
 
+// simple character-by-character output
+// as string_view may not be NUL terminated
 inline void vprint(std::string_view fmt) {
-    for(auto c : fmt){
-        Putchar(c);
-    }
+  for (auto c : fmt) {
+    Putchar(c);
+  }
 }
 
-inline void print_arg(const char* p) { vprint(p); }
+// overload to match NTBS decay
+inline void print_arg(const char *p) { vprint(p); }
 
+// overload to match string or string_view
 inline void print_arg(std::string_view p) { vprint(p); }
 
-template <typename T>
-void print_arg(T p) {
-    static constexpr std::size_t buff_size{16};
-    std::array<char, buff_size> buff{};
-    if constexpr (std::is_integral_v<T>) {
-        snprintf(buff.data(), buff.size(), "%d", p);
-    }
-    if constexpr (std::is_floating_point_v<T>) {
-        snprintf(buff.data(), buff.size(), "%f", p);
-    }
-    buff.back() = '\0';  // guartentee NTBS
-    vprint(std::string_view(buff.data(), buff.size()));
+template <typename T> void print_arg(T p) {
+  static constexpr std::size_t buff_size{16};
+  std::array<char, buff_size> buff{};
+  if constexpr (std::is_integral_v<T>) {
+    auto count = snprintf(buff.data(), buff.size(), std::numeric_limits<T>::is_signed?"%d":"%u", p);
+    vprint(std::string_view(buff.data(), count));
+  }
+  if constexpr (std::is_floating_point_v<T>) {
+    auto count = snprintf(buff.data(), buff.size(), "%f", p);
+    vprint(std::string_view(buff.data(), count));
+  }
 }
 
 // #include <charconv>
@@ -46,31 +50,35 @@ void print_arg(T p) {
 //     }
 // }
 
-template <typename... T>
-inline void print(std::string_view fmt, T&&... args);
+template <typename... T> inline void print(std::string_view fmt, T &&...args);
 
 template <typename T1, typename... T2>
-inline void vprint(std::string_view fmt, T1&& p, T2&&... args) {
-    print_arg(p);
-    print(fmt, args...);
+inline void vprint(std::string_view fmt, T1 &&p, T2 &&...args) {
+  print_arg(p);
+  print(fmt, args...);
 }
 
-template <typename... T>
-inline void print(std::string_view fmt, T&&... args) {
+template <typename... T> inline void print(std::string_view fmt, T &&...args) {
+  constexpr auto arg_count = sizeof...(T);
+  if constexpr (arg_count == 0) {
+    vprint(fmt);
+  }
+  if constexpr (arg_count != 0) {
     if (auto pos = fmt.find('{'); pos == std::string_view::npos) {
-        vprint(fmt);
+      vprint(fmt);
     } else {
-        vprint(fmt.substr(0, pos));
-        if (auto epos = fmt.find('}'); pos != std::string_view::npos) {
-            vprint(fmt.substr(epos + 1), args...);
-        }
+      vprint(fmt.substr(0, pos));
+      if (auto epos = fmt.find('}'); pos != std::string_view::npos) {
+        vprint(fmt.substr(epos + 1), args...);
+      }
     }
+  }
 }
 
 template <typename... T>
-inline void println(std::string_view fmt, T&&... args) {
-    print(fmt, args...);
-    putchar('\n');
+inline void println(std::string_view fmt, T &&...args) {
+  print(fmt, args...);
+  Putchar('\n');
 }
 
-}  // namespace embedded::fmt
+} // namespace embedded::fmt
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,69 @@
-#include "efmt.hpp"
+#include <cassert>
+#include <iostream>
+#include <string>
 
+std::string test_string{};
+auto Putstr = [&str = test_string](auto c) { str += c; };
+auto print_test_string = [](auto& str){ for(auto c:str) { putchar('.'), putchar(c); }};
+
+#define Putchar(x) Putstr(x)
+
+#include "efmt.hpp"
 using namespace embedded;
 
 int main() {
-    fmt::print("Hello, world! 1\n");
-    fmt::println("Hello, world! 2");
-    fmt::print("Hello, {} {}\n", "world!", 3);
-    fmt::print("Default format: {} {} {}\n", 42, 1.2f, 2);
-    puts("");
+
+  // case 1 - simple string
+  fmt::print("Hello, world!\n");
+  std::cout << test_string;
+  assert(test_string == "Hello, world!\n");
+  test_string.clear();
+  // case 2 - simple string
+  fmt::print("Hello, {}\n", "world!");
+  std::cout << test_string;
+  assert(test_string == "Hello, world!\n");
+  test_string.clear();
+  // case 3
+  fmt::print("int: {}\n", 42);
+  std::cout << test_string;
+  assert(test_string == "int: 42\n");
+  test_string.clear();
+  // case 4
+  fmt::print("float: {}\n", 1.2f);
+  std::cout << test_string;
+  assert(test_string == "float: 1.200000\n");
+  test_string.clear();
+    // case 5
+  fmt::print("unsigned: {}\n", static_cast<unsigned int>(0xFFFF'FFFFU));
+  std::cout << test_string;
+  assert(test_string == "unsigned: 4294967295\n");
+  test_string.clear();
+    // case 6
+  fmt::print("signed: {}\n", static_cast<int>(0xFFFF'FFFF));
+  std::cout << test_string;
+  assert(test_string == "signed: -1\n");
+  test_string.clear();
+  ///////////////////////////////////////////////////////////
+
+  // outliers (1) - arg but no {}
+  fmt::print("int: \n", 42);
+  std::cout << test_string;
+  assert(test_string == "int: \n");
+  test_string.clear();
+  // outliers (2) -  {} but no arg
+  fmt::print("int: {}\n");
+  std::cout << test_string;
+  assert(test_string == "int: {}\n");
+  test_string.clear();
+  // outliers (3) -  {} but no arg
+  fmt::print("int: {} {}\n", 42);
+  std::cout << test_string;
+  assert(test_string == "int: 42 {}\n");
+  test_string.clear();
+  // outliers (4) -  {} but no arg
+  fmt::print("int: {}\n", 42, 24);
+  std::cout << test_string;
+  assert(test_string == "int: 42\n");
+  test_string.clear();
+  puts("");
 }
